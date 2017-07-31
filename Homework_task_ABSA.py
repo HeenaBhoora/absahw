49382a7982dce8c3969f8453f0e24e5a01b7c253
HeenaBhoora <30590078+HeenaBhoora@users.noreply.github.com>
2017-07-31 10:26:51 +0200

    Add files via upload

---
+++ Homework_task_ABSA.py
@@ -0,0 +1,53 @@
+
+
+# Hello
+
+import requests
+URL = 'https://za.pinterest.com/login/?referrer=home_page'
+PARAMS = {'Email or phone number': 'heenab27@hotmail.com', 'Password': 'Absa123'}
+requests.get(url=URL, params=PARAMS)
+print ('Server Operational')
+
+
+#Registration
+
+def users_signup(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        pwd_1 = request.POST.get('password1')
+        pwd_2 = request.POST.get('password2')
+        if pwd_1 == pwd_2:
+             user = User.objects.create_user(
+                                              username=username,
+                                              email=email,
+                                              password=pwd_1,
+                                             )
+             return HttpResponseRedirect("/")
+        else:
+             error = " Passwords do not match "
+             return render(request, 'login/signup.html',{"error":error})
+    else:
+         return render(request, 'login/signup.html')
+
+
+#Login
+
+		
+with requests.Session() as c:
+    c.get(URL)
+    csrftoken = c.cookies['csrftoken']
+    payload = {
+        'csrfmiddlewaretoken': 'csrftoken',
+        'Username': 'heenab27@hotmail.com',
+        'Password': 'Absa123'
+}
+    p = c.post('https://za.pinterest.com/login/?referrer=home_page', data=payload, headers={"Referer":"https://za.pinterest.com/"})
+    print ('csrftoken is', csrftoken)
+    
+
+#Authentication
+from requests.auth import HTTPDigestAuth
+url = 'http://httpbin.org/digest-auth/auth/user/pass'
+x = requests.get(url, auth=HTTPDigestAuth('user', 'pass'))
+print (x.text)
